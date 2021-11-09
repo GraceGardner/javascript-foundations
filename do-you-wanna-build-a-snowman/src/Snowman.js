a8e35141bf5801c3c26e9d96ba9cc1f8a02f00fb
Grace Gardner <graceirisgardner@gmail.com>
2021-11-09 14:39:31 -0700

    Complete snowman test

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,17 @@
 class Snowman {
+  constructor(snowman){
+    this.carrots = snowman.carrots;
+    this.coal = snowman.coal;
+    this.buttons = snowman.buttons;
+    this.snowballs = snowman.snowballs;
+    this.magicHat = false;
+  }
 
+  canWearMagicHat(){
+    if (this.coal == 2 && this.buttons > 4 && this.carrots > 0 && this.snowballs > 1) {
+    this.magicHat = true;
+  }
+  }
 }
 
 module.exports = Snowman;
