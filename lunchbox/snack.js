0346be3113cd0dc21ad68cc36fb2285a14ec922b
Grace Gardner <graceirisgardner@gmail.com>
2021-11-09 11:33:58 -0700

    Complete snack test

--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,25 @@
 class Snack {
+constructor(snack) {
+  this.deliciousLevel = "extra";
+  this.type = snack;
+  this.amount = 100;
+  this.isInLunchBox = false;
+  this.cuttingItClose = false;
+}
 
+getEaten() {
+  this.amount -= 10;
+  if (this.amount < 30){
+    this.cuttingItClose = true;
+  }
+}
+
+checkForHealthy() {
+  if (this.type.includes("fruit") || this.type.includes("Fruit")) {
+    return true;
+  }
+  return false;
+}
 }
 
 module.exports = Snack;
